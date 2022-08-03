ec93b57b131a6cbabbed04c4dcf6263df789965d
Jackson Chen <momonbin1991@icloud.com>
2022-08-03 20:43:20 +0800

    20220803修正錯誤之前存檔

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,13 +3,18 @@ import ChartBar from './ChartBar';
 import './Chart.css';
 
 const Chart = props => {
+	// const totalMaximum = Math.max(1, 2, 3);
+	/* 在上述例子 數字組中，取出最大值，但是現在卻是一個對象數組 */
+	const dataPointValues = props.dataPoints.map(datapoint => datapoint.value);
+	const totalMaximum = Math.max(...dataPointValues);
+
 	return (
 		<div className="chart">
 			{props.dataPoints.map(datapoint => {
 				<ChartBar
 					key={datapoint.label}
 					value={datapoint.value}
-					maxValue={null}
+					maxValue={totalMaximum}
 					label={datapoint.label}
 				/>;
 			})}
--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+import './Expenses.css';
 
 const Expenses = props => {
 	const [filteredYear, setFilteredYear] = useState('2020');
@@ -24,6 +25,7 @@ const Expenses = props => {
 					selected={filteredYear}
 					onChangeFilter={filterChangeHandler}
 				/>
+				<ExpensesChart expenses={filteredExpenses} />
 				<ExpensesList items={filteredExpenses} />
 			</Card>
 		</div>
